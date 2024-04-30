fdd15572d936bdbadaf65401edd7f3ee40e71c5e
Nathanael Tan <nathanaeltan@Nathanaels-MacBook-Pro.local>
2024-04-30 10:25:29 +0800

    update docker compose command to run migrations

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a9*g0*t*x#+&+vli24k#)(v+wf4!!c!7rizh83$jdb!ldnsj^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
